f606b51eb25dd01ba437ccfd2156d277b3618731
Juan-Casiano <juan_3127240893@utd.edu.mx>
2025-07-07 09:38:15 -0600

    Delete PP1 directory

--- PROGRAMACION_ESTRUCTURADA_2025/P1/2_modulos_paquetes/2_modulos_paquetes/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras Ruano"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos2()
-print(f"\t .:: Agenda telefonica ::. \n\t\tNombre:{nom}\n\t\tTelefoo:{tel}")
-modulos.espereTecla()
